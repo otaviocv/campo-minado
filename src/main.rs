30cfafcec0ef0094a5344d95f7dd497ffcb0081c
Otavio Vasques <otavio.vasques@nubank.com.br>
2024-04-21 15:59:39 -0300

    Add resources

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
+use bevy::window::PrimaryWindow;
 
 use rand::seq::SliceRandom;
 use rand::thread_rng;
@@ -6,68 +8,37 @@ use std::fmt::Display;
 use std::fmt::Formatter;
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    App::new().add_plugins((DefaultPlugins, MyPlugin)).run();
 }
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (add_people, build_board));
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don’t need to change any other names
-        }
-    }
-}
-
-fn hello_world() {
-    println!("hello world!");
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
+const CELL_SIZE: f32 = 40.0;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-enum Value {
+enum BoardValue {
     Empty,
     Bomb,
     Clue(i32),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Resource)]
 struct Board {
     height: i32,
     width: i32,
-    values: Vec<Vec<Value>>,
+    values: Vec<Vec<BoardValue>>,
 }
 
-impl Display for Value {
+#[derive(Debug, Resource)]
+struct BoardInstance;
+
+#[derive(Debug, Resource)]
+struct BoardMaskInstance;
+
+impl Display for BoardValue {
     fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
         match self {
-            Value::Empty => write!(formatter, "."),
-            Value::Bomb => write!(formatter, "B"),
-            Value::Clue(clue) => write!(formatter, "{}", clue),
+            BoardValue::Empty => write!(formatter, "."),
+            BoardValue::Bomb => write!(formatter, "B"),
+            BoardValue::Clue(clue) => write!(formatter, "{}", clue),
         }?;
 
         Ok(())
@@ -88,14 +59,31 @@ impl Display for Board {
     }
 }
 
-fn build_board() {
-    let height = 9;
-    let width = 9;
-    let number_of_bombs = 10;
+pub struct MyPlugin;
 
-    let board = build_random_board(&height, &width, &number_of_bombs);
-    let board_with_clues = build_clues(board);
-    println!("{}", board_with_clues)
+impl Plugin for MyPlugin {
+    fn build(&self, app: &mut App) {
+        let height = 9;
+        let width = 9;
+        let number_of_bombs = 10;
+
+        let board = build_random_board(&height, &width, &number_of_bombs);
+        let board_with_clues = build_clues(&board);
+        println!("{}", board_with_clues);
+
+        let board_mask = build_mask(&height, &width);
+        println!("{}", board_mask);
+
+        app.insert_resource(board_with_clues)
+            .insert_resource(board_mask)
+            .insert_resource(GridPosition { position: None })
+            .add_systems(Startup, add_camera)
+            .add_systems(Update, (update_grid_postition, draw_board).chain());
+    }
+}
+
+fn add_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
 
 fn build_random_board(height: &i32, width: &i32, number_of_bombs: &i32) -> Board {
@@ -110,12 +98,12 @@ fn build_random_board(height: &i32, width: &i32, number_of_bombs: &i32) -> Board
     coordinates.shuffle(&mut thread_rng());
     let bombs = coordinates[0usize..*number_of_bombs as usize].to_vec();
 
-    let mut board: Vec<Vec<Value>> = (0..*height)
-        .map(|_| (0..*width).map(|_| Value::Empty).collect())
+    let mut board: Vec<Vec<BoardValue>> = (0..*height)
+        .map(|_| (0..*width).map(|_| BoardValue::Empty).collect())
         .collect();
 
     for bomb in bombs.iter() {
-        board[bomb.0 as usize][bomb.1 as usize] = Value::Bomb;
+        board[bomb.0 as usize][bomb.1 as usize] = BoardValue::Bomb;
     }
 
     Board {
@@ -125,11 +113,11 @@ fn build_random_board(height: &i32, width: &i32, number_of_bombs: &i32) -> Board
     }
 }
 
-fn build_clues(board: Board) -> Board {
+fn build_clues(board: &Board) -> Board {
     let mut new_board = board.values.clone();
     for i in 0..board.height {
         for j in 0..board.width {
-            if board.values[i as usize][j as usize] == Value::Bomb {
+            if board.values[i as usize][j as usize] == BoardValue::Bomb {
                 continue;
             }
             let neighbors = get_neighbors(&i, &j);
@@ -140,13 +128,13 @@ fn build_clues(board: Board) -> Board {
                     .get(neighbor.0 as usize)
                     .unwrap_or(&vec![])
                     .clone();
-                let neighbor_value = row.get(neighbor.1 as usize).unwrap_or(&Value::Empty);
-                if neighbor_value == &Value::Bomb {
+                let neighbor_value = row.get(neighbor.1 as usize).unwrap_or(&BoardValue::Empty);
+                if neighbor_value == &BoardValue::Bomb {
                     clue += 1;
                 }
 
                 if clue > 0 {
-                    new_board[i as usize][j as usize] = Value::Clue(clue);
+                    new_board[i as usize][j as usize] = BoardValue::Clue(clue);
                 }
             }
         }
@@ -154,7 +142,8 @@ fn build_clues(board: Board) -> Board {
 
     Board {
         values: new_board,
-        ..board
+        height: board.height.clone(),
+        width: board.width.clone(),
     }
 }
 
@@ -169,4 +158,153 @@ fn get_neighbors(row_index: &i32, column_index: &i32) -> Vec<(i32, i32)> {
         (row_index + 1, *column_index),
         (row_index + 1, column_index + 1),
     ]
+}
+
+#[derive(Clone, PartialEq, Eq)]
+enum MaskValue {
+    Closed,
+    Open,
+    Flagged,
+    Question,
+}
+
+#[derive(Resource)]
+struct BoardMask {
+    height: i32,
+    width: i32,
+    values: Vec<Vec<MaskValue>>,
+}
+
+fn build_mask(height: &i32, width: &i32) -> BoardMask {
+    let board: Vec<Vec<MaskValue>> = (0..*height)
+        .map(|_| (0..*width).map(|_| MaskValue::Closed).collect())
+        .collect();
+
+    BoardMask {
+        height: height.clone(),
+        width: width.clone(),
+        values: board,
+    }
+}
+
+impl Display for MaskValue {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            MaskValue::Closed => write!(formatter, "."),
+            MaskValue::Open => write!(formatter, " "),
+            MaskValue::Flagged => write!(formatter, "F"),
+            MaskValue::Question => write!(formatter, "?"),
+        }?;
+
+        Ok(())
+    }
+}
+
+impl Display for BoardMask {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(formatter, "{}x{}\n", self.height, self.width)?;
+
+        for i in 0..self.height {
+            for j in 0..self.width {
+                write!(formatter, "{}", self.values[i as usize][j as usize])?;
+            }
+            write!(formatter, "\n")?;
+        }
+        Ok(())
+    }
+}
+
+fn draw_board(
+    mut commands: Commands,
+    mut board: Res<Board>,
+    mut mask: Res<BoardMask>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    grid_position: Res<GridPosition>,
+) {
+    let green = Color::hsl(90.0, 0.95, 0.7);
+    let red = Color::hsl(0.0, 0.95, 0.7);
+    let padding = 0.0;
+
+    let font = asset_server.load("fonts/Swansea.ttf");
+    let text_justification = JustifyText::Center;
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: 10.0,
+        color: Color::WHITE,
+    };
+    let start_x = -CELL_SIZE * board.width as f32 / 2.0;
+    let start_y = CELL_SIZE * board.height as f32 / 2.0;
+    for i in 0..board.height {
+        for j in 0..board.width {
+            let value = board.values[i as usize][j as usize].clone();
+            let mask_value = mask.values[i as usize][j as usize].clone();
+
+            if mask_value == MaskValue::Open {
+                commands.spawn(Text2dBundle {
+                    text: Text::from_section(value.to_string(), text_style.clone())
+                        .with_justify(text_justification),
+                    transform: Transform::from_xyz(
+                        start_x + j as f32 * (CELL_SIZE + padding),
+                        start_y - i as f32 * (CELL_SIZE + padding),
+                        1.0,
+                    ),
+                    ..default()
+                });
+            }
+
+            let mut color = green;
+            if let Some(grid_coordinates) = &grid_position.position {
+                if grid_coordinates.x == j && grid_coordinates.y == i {
+                    color = red;
+                    println!("Position {:?} is red", grid_coordinates);
+                }
+            }
+
+            let shape = Mesh2dHandle(meshes.add(Rectangle::new(CELL_SIZE, CELL_SIZE)));
+            commands.spawn(MaterialMesh2dBundle {
+                mesh: shape,
+                material: materials.add(color),
+                transform: Transform::from_xyz(
+                    start_x + j as f32 * (CELL_SIZE + padding),
+                    start_y - i as f32 * (CELL_SIZE + padding),
+                    0.0,
+                ),
+                ..default()
+            });
+        }
+    }
+}
+
+#[derive(Debug)]
+struct GridCoordinates {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Resource, Debug)]
+struct GridPosition {
+    position: Option<GridCoordinates>,
+}
+
+fn update_grid_postition(
+    mut position: ResMut<GridPosition>,
+    board: Res<Board>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window_size = Vec2::new(q_windows.single().width(), q_windows.single().height());
+    if let Some(cursor_position) = q_windows.single().cursor_position() {
+        let world_position = Vec2::new(
+            cursor_position.x - window_size.x / 2.,
+            cursor_position.y - window_size.y / 2.,
+        );
+        let board_coordinates = GridCoordinates {
+            x: (0.5 + (board.width as f32 / 2.0) + world_position.x / CELL_SIZE) as i32,
+            y: (0.5 + (board.height as f32 / 2.0) + world_position.y / CELL_SIZE) as i32,
+        };
+        position.position = Some(board_coordinates);
+    } else {
+        position.position = None;
+    }
 }
